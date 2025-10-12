7ee98e2c63339484c1d18570639a18b03ffeade9
Jigyasa Upadhyay <jigyasaupadhyay60@gmail.com>
2025-10-12 23:12:38 +0530

    css updated

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,15 @@
-console.log("hi")
+function handleBotClick(){
+    const elem = document.getElementById("chat_body")
+    const agentIcon = document.getElementById("chatIcon")
+    elem.classList.remove("hidden")
+    agentIcon.classList.add("hidden")
+
+}
+
+function handleBotClose() {
+    const elem = document.getElementById("chat_body")
+    const agentIcon = document.getElementById("chatIcon")
+    elem.classList.add("hidden")
+    agentIcon.classList.remove("hidden")
+
+}
